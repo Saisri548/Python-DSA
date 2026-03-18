7bb5abe0842a34ce6c86835ce9382f36a3eaec13
saisri548 <thotasaisri7@gmail.com>
2026-03-18 19:29:01 +0530

    Arrays

---
+++ lists.py
@@ -0,0 +1,12 @@
+#list intro and imp methods and imp problems 
+arr=[10,20,30,40,50]
+empty=[]
+mixed=["hi","Priya",10,30]
+print(arr[0])
+print(arr[1])
+print(mixed[2])
+arr1=[1,2,3,4,5,6]
+arr1.append(2)
+arr1.append(10)
+arr1.insert(1,10)
+print(arr1,arr,mixed)
